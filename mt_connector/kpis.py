6129403d507d69b09460dc14bd193f5f45f008ef
Kevin Bruton <kevin.jose.bs@gmail.com>
2022-07-30 00:39:13 +0200

    feat: kpi calc update

--- mt_connector/kpis.py
+++ mt_connector/kpis.py
@@ -17,7 +17,7 @@ def get_max_dd (balances):
 def get_bt_kpis (btStart, btEnd, trades, deposit=10000):
   time_str_format = '%Y-%m-%d %H:%M:%S'
   start = datetime.combine(datetime.strptime(btStart, time_str_format), datetime.min.time())
-  end = datetime.strptime(btEnd, time_str_format)
+  end = datetime.combine(datetime.strptime(btEnd, time_str_format), datetime.min.time())
   return get_kpis(start, end, trades, deposit)
 
 def get_demo_kpis (start, trades, deposit=1000):
@@ -53,9 +53,9 @@ def get_kpis (start, end, trades, deposit):
     elif p < 0:
       gross_loss -= p
   profit_factor = dec2(gross_profit / abs(gross_loss))
-  num_days_on_demo = (end - start).total_seconds() / 60 / 60 / 24
-  annual_pct_ret = dec2(total_pct_ret * 365 / num_days_on_demo)
-  max_dd = get_max_dd(balances)
+  num_days = int((end - start).total_seconds() / 60 / 60 / 24)
+  annual_pct_ret = dec2(total_pct_ret * 365 / num_days)
+  max_dd = dec2(get_max_dd(balances))
   max_pct_dd = dec2(max_dd / deposit * 100)
   annual_pct_ret_vs_dd_pct = dec2(annual_pct_ret / max_pct_dd)
   num_wins = 0
@@ -63,19 +63,22 @@ def get_kpis (start, end, trades, deposit):
     if p > 0:
       num_wins += 1
   win_pct = dec2(num_wins / len(profit) * 100)
-  print('START:', start, '; END:', end)
-  print('BALANCE START / END:', balances[0], balances[-1])
-  print('GROSS PROFIT / LOSS:', gross_profit, gross_loss)
-  print('PCT RET:', total_pct_ret)
-  print('NUM DAYS ON DEMO:', num_days_on_demo)
-  return {
+  # print('    START:', start, '; END:', end)
+  # print('    BALANCE START / END:', balances[0], dec2(balances[-1]))
+  # print('    GROSS PROFIT / LOSS:', dec2(gross_profit), dec2(gross_loss))
+  # print('    PCT RET:', total_pct_ret)
+  # print('    NUM DAYS:', num_days)
+  kpis = {
     'annualPctRet': annual_pct_ret,
     'maxDD': max_dd,
     'maxPctDD': max_pct_dd,
     'annPctRetVsDdPct': annual_pct_ret_vs_dd_pct,
     'winPct': win_pct,
-    'profitFactor': profit_factor
+    'profitFactor': profit_factor,
+    'numTrades': len(trades)
   }
+  print('    ', kpis)
+  return kpis
 
 
 
