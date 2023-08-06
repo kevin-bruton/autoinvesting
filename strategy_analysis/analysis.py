43c7a4d340645dbe4d6740c09c1a2ac0e5b3a8fa
=Kevin Bruton <=>
2023-08-07 01:56:21 +0200

    feat: add more config options to generated strategies analysis for workflow optimization

--- strategy_analysis/analysis.py
+++ strategy_analysis/analysis.py
@@ -5,11 +5,27 @@ import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
 import pandas as pd
 
-strategy_dir = '/home/kevin.bruton/.wine/drive_c/sqx/user/projects/EURUSD - H1 Simple/databanks/Results/'
+strategy_dir = '/home/kevin.bruton/.wine/drive_c/sqx137_4/user/projects/EURUSD - H1 Simple/databanks/Results/'
 
 strategy_files = [f for f in os.listdir(strategy_dir) if os.path.isfile(strategy_dir + f)]
 
-def indicator_analysis ():
+def put_values_in_range_and_plot_bar_chart(values, block_width):
+  max_value = max(values)
+  range_frequencies = {}
+  for start_value in range(int(max_value / block_width) + 1):
+     range_frequencies[start_value * block_width] = 0
+  
+  for value in values:
+    range_start = int(value / block_width) * block_width
+    range_frequencies[range_start] += 1
+
+  df = pd.DataFrame(range_frequencies.items(), columns=['Range', 'Count']).sort_values('Range')
+  df.plot.bar(x="Range", y="Count")
+  plt.show()
+
+# This analyses the building blocks used in all strategies
+# and shows the ones that have been used more times than the threshold defined
+def indicator_analysis (threshold=10):
   indicators = {}
   for strategy_file in strategy_files:
       with ZipFile(strategy_dir + strategy_file, 'r') as zipfile:
@@ -32,34 +48,50 @@ def indicator_analysis ():
   print('\nHIGH FREQUENCY INDICATORS:\n')
   num_selected = 0
   for indicator in indicators.keys():
-    if indicators[indicator]['count'] >= 10:
+    if indicators[indicator]['count'] >= threshold:
       num_selected += 1
       print(indicator, indicators[indicator])
 
   print('\nNum selected:', num_selected)
 
-def sl_fixed_size_analysis(direction='Long'):
-  sl = []
+def get_node_from_strategy_files (xpath_to_node, action):
   for strategy_file in strategy_files:
     with ZipFile(strategy_dir + strategy_file, 'r') as zipfile:
       with zipfile.open('strategy_Portfolio.xml') as xmlfile:
         root = ET.fromstring(xmlfile.read())
-        sl_value = root.find(f'.//Rule[@name="{direction} entry"]//Param[@key="#StopLoss.StopLoss#"]//Param').text
-        sl.append(float(sl_value))
+        node = root.find(xpath_to_node)
+        action(node)
 
-  # Get max value & setup range_frequencies
-  max_sl = max(sl)
-  block_width = 5
-  range_frequencies = {}
-  for start_sl in range(int(max_sl / 5) + 1):
-     range_frequencies[start_sl * block_width] = 0
-  
-  for sl_value in sl:
-    range_start = int(sl_value / block_width) * block_width
-    range_frequencies[range_start] += 1
+def sl_fixed_size_analysis(direction='Long'):
+  sl = []
+  action = lambda node: sl.append(float(node.text))
+  xpath = f'.//Rule[@name="{direction} entry"]//Param[@key="#StopLoss.StopLoss#"]//Param'
+  get_node_from_strategy_files(xpath, action)
+  put_values_in_range_and_plot_bar_chart(values=sl, block_width=5)
 
-  df = pd.DataFrame(range_frequencies.items(), columns=['Range', 'Count']).sort_values('Range')
-  df.plot.bar(x="Range", y="Count")
-  plt.show()
+def sl_percentage_based_analysis(direction='Long'):
+  sl = []
+  action = lambda node: sl.append(float(node.text))
+  xpath = f'.//Rule[@name="{direction} entry"]//Param[@key="#StopLoss.StopLoss#"]//Param'
+  get_node_from_strategy_files(xpath, action)
+  put_values_in_range_and_plot_bar_chart(values=sl, block_width=10)
 
-sl_fixed_size_analysis()
+def sl_atr_based_analysis(direction='Long'):
+  sl = []
+  action = lambda node: sl.append(float(node.text))
+  xpath = f'.//Rule[@name="{direction} entry"]//Param[@key="#StopLoss.StopLoss#"]//Param[@key="#Value#"]'
+  get_node_from_strategy_files(xpath, action)
+  put_values_in_range_and_plot_bar_chart(values=sl, block_width=25)
+
+def tp_atr_based_analysis(direction='Long'):
+  sl = []
+  action = lambda node: sl.append(float(node.text))
+  xpath = f'.//Rule[@name="{direction} entry"]//Param[@key="#ProfitTarget.ProfitTarget#"]//Param[@key="#Value#"]'
+  get_node_from_strategy_files(xpath, action)
+  put_values_in_range_and_plot_bar_chart(values=sl, block_width=25)
+
+# indicator_analysis()
+# sl_fixed_size_analysis()
+# sl_percentage_based_analysis()
+# sl_atr_based_analysis()
+tp_atr_based_analysis()
