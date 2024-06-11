5ffbd27000792364f22cb1f925c96d13155879b7
Kevin Bruton <kevin.jose.bs@gmail.com>
2024-06-11 17:11:41 +0200

    feat: add log for wifi connection issues

--- scripts/wifi_connector.py
+++ scripts/wifi_connector.py
@@ -2,14 +2,19 @@ import requests
 import winwifi
 from datetime import datetime
 
+def log(txt):
+  time_str = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+  with open(f"logs/master.log", 'a') as f:
+    f.write(f"{time_str} {txt}\n")
+
 def test_connection():
   try:
     resp = requests.get('http://www.google.com', timeout=2)
     if resp.status_code != 200:
-      print('Failed to connect to google.com. Connection status code:', resp.status_code)
+      log('Failed http test. Connection status code:', resp.status_code)
     return resp.status_code == 200
   except:
-    print('Failed to connect to google.com')
+    log('Failed http test. Connection error.')
     return False
   
 def connect_to_wifi(ssid):
@@ -24,13 +29,13 @@ def connect_to_wifi(ssid):
 def check_connection():
   wifi_names = ['Oz2', 'Factory']
   if not test_connection():
-    print(datetime.now(), 'No internet connection. Trying to connect to ', wifi_names[0])
+    log('Failed http test. Now trying to connect to ', wifi_names[0])
     if not connect_to_wifi(wifi_names[0]):
-      print(datetime.now(), 'Failed to connect to ', wifi_names[0], '. Trying to connect to ', wifi_names[1])
+      log('Failed to connect to ', wifi_names[0], '. Trying to connect to ', wifi_names[1])
       if not connect_to_wifi(wifi_names[1]):
-        print(datetime.now(), 'Failed to connect to ', wifi_names[1])
+        log('Failed to connect to ', wifi_names[1])
         return False
       else:
-        print(datetime.now(), 'Connected to ', wifi_names[1])
+        log('Connected to ', wifi_names[1])
     else:
-      print(datetime.now(), 'Connected to ', wifi_names[0])
+      log('Connected to ', wifi_names[0])
