769d1b0634028d0bd4b4dbb658d68b25d74fb854
Kevin Bruton <kevin.jose.bs@gmail.com>
2024-06-14 18:05:02 +0200

    feat: add missing deps to Pipfile

--- scripts/cron.py
+++ scripts/cron.py
@@ -1,14 +1,17 @@
 import schedule
 import time
 from scripts.update_from_mt import run_update_from_mt
-from scripts.wifi_connector import check_connection
+import platform
+if platform.system() == 'Windows':
+  from scripts.wifi_connector import check_connection
 
 def run_cron():
   update_freq_secs = 60 * 60
   print('\nStarting cron jobs...\n')
 
   schedule.every(update_freq_secs).seconds.do(run_update_from_mt)
-  schedule.every(60).seconds.do(check_connection)
+  if platform.system() == 'Windows':
+    schedule.every(60).seconds.do(check_connection)
 
   while True:
     schedule.run_pending()
