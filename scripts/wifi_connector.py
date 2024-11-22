d169eca702e3b7e1d9d627607f058c1be2126123
Kevin Bruton <kevin.jose.bs@gmail.com>
2024-11-22 11:59:56 +0100

    feat: add wifi connection change loggin

--- scripts/wifi_connector.py
+++ scripts/wifi_connector.py
@@ -9,6 +9,24 @@ def _log(txt):
   with open(f"logs/wifi.log", 'a') as f:
     f.write(f"{time_str} {txt}\n")
 
+def _get_last_wifi_status():
+  try:
+    with open(f'logs/wifi_connection_status.txt', 'r') as f:
+      return f.read().strip().split(';')[-1]
+  except:
+    return 'None'
+  
+def _save_wifi_status(status):
+  time_str = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+  with open(f'logs/wifi_connection_status.txt', 'w') as f:
+    f.write(f'{time_str};{status}')
+
+def _get_wifi_connected_to():
+  interfaces = winwifi.WinWiFi.get_connected_interfaces()
+  if not interfaces or len(interfaces) == 0:
+    return 'None'
+  return interfaces[0].ssid
+
 def _test_connection():
   try:
     resp = requests.get('http://www.google.com', timeout=2)
@@ -32,6 +50,11 @@ def _connect_to_wifi(ssid):
   return _test_connection()
 
 def check_connection():
+  last_status = _get_last_wifi_status()
+  current_status = _get_wifi_connected_to()
+  if last_status != current_status:
+    _save_wifi_status(current_status)
+    _log(f'Wifi status changed from {last_status} to {current_status}')
   if not _test_connection():
     _log('Failed http test. Now trying to connect to ' + wifi_names[0])
     if not _connect_to_wifi(wifi_names[0]):
